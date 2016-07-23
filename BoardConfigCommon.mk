674c9d4059b91efee44ba422c454eda4c3d9a32c
Kousuke Nagatsuka <koron393@gmail.com>
2016-07-23 23:10:51 +0900

    msm8974-common: Move cpuquiet properties to platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,8 +112,6 @@ CM_POWERHAL_EXTENSION := qcom
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-TARGET_SYSTEM_PROP := device/sony/msm8974-common/system.prop
-
 # SELinux
 include device/sony/sepolicy/sepolicy.mk
 
