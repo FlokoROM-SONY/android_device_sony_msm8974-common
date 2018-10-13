59a57381dbb86c854b8e00d76f2ad61e32ad439a
koron393 <koron393@gmail.com>
2018-10-13 18:41:08 +0900

    msm8974-common: hidl.mk: Fix spaces

--- hidl.mk
+++ hidl.mk
@@ -1,37 +1,37 @@
 # Audio
 PRODUCT_PACKAGES += \
-	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl
-	
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
 # Bluetooth
 PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl \
-	android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
-	android.hardware.camera.provider@2.4-impl \
-	camera.device@1.0-impl
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl 
+    android.hardware.drm@1.0-impl
 
 # Display
 PRODUCT_PACKAGES += \
-	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.mapper@2.0-impl \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.composer@2.1-service
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
 
 # Dumpstate
 PRODUCT_PACKAGES += \
-	android.hardware.dumpstate@1.0-service.sony
+    android.hardware.dumpstate@1.0-service.sony
 
 # FM
 PRODUCT_PACKAGES += \
-	android.hardware.broadcastradio@1.0-impl
+    android.hardware.broadcastradio@1.0-impl
 
 # GNSS
 PRODUCT_PACKAGES += \
@@ -45,27 +45,27 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-	android.hardware.keymaster@3.0-impl \
-	android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
-	android.hardware.light@2.0-impl \
-	android.hardware.light@2.0-service
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Memtrack
 PRODUCT_PACKAGES += \
-	android.hardware.memtrack@1.0-impl \
-	android.hardware.memtrack@1.0-service
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # NFC
 PRODUCT_PACKAGES += \
-	android.hardware.nfc@1.0-impl
+    android.hardware.nfc@1.0-impl
 
 # Netd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0
-    
+
 # Netutils
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
@@ -76,7 +76,7 @@ PRODUCT_PACKAGES += \
 
 # Rendering
 PRODUCT_PACKAGES += \
-	android.hardware.renderscript@1.0-impl
+    android.hardware.renderscript@1.0-impl
 
 #RIL
 PRODUCT_PACKAGES += \
@@ -95,13 +95,13 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-service \
-	android.hardware.usb@1.0-service.msm8974-common \
+    android.hardware.usb@1.0-service \
+    android.hardware.usb@1.0-service.msm8974-common \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl
 
 # WiFi
 PRODUCT_PACKAGES += \
-	android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service
