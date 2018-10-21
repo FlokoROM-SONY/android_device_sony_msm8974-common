11ddd5557404c8e6f18f310c748ca77b8d13af24
koron393 <koron393@gmail.com>
2018-10-21 19:34:54 +0900

    msm8974-common: Add missing wpa_supplicant_overlay.conf
    * Add wpa_supplicant_overlay.conf
    * Add p2p_supplicant_overlay.conf
    * Add p2p_supplicant.conf

--- msm8974.mk
+++ msm8974.mk
@@ -166,4 +166,9 @@ PRODUCT_PACKAGES += \
     wificond \
     wifilogd \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    p2p_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/rootdir/system/vendor/etc/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
