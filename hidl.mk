b13eb504f44a50f4d24a27b8c6e8bf1f022ee888
YumeMichi <do4suki@gmail.com>
2019-09-11 10:44:44 +0800

    msm8974-common: wifi: Adatpted wifi HIDL service
    Change-Id: I443fce88e4db5a4f1896ff13a60683a83d72903c
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.3 \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service.sony
 
 # Trust
 PRODUCT_PACKAGES += \
