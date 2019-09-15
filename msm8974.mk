ed9f8add3c0c4847aae70ca9878db2f799f35168
koron393 <koron393@gmail.com>
2019-09-16 00:30:33 +0900

    msm8974-common: Remove double defined gps.conf
    * Fix build in Q
    
    Change-Id: If802f591019d01f9248ab8b674a51c3a889e4721

--- msm8974.mk
+++ msm8974.mk
@@ -64,7 +64,6 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(COMMON_PATH)/gps/sap.conf:system/etc/sap.conf \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
